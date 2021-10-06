daaf543dbd3f6cbdae30b7f9b61f24a8eef82bb5
Mkisecok <mahmutkisecok@gmail.com>
2021-10-06 16:01:57 +0200

    Add task3

--- classes.js
+++ classes.js
@@ -60,3 +60,77 @@ const howMuchCylinder= new Volume(5,5);
 console.log(howMuchCylinder.ourVolume());
 
 console.log('='.repeat(50)+' Task 3');
+
+class Tv
+{
+    constructor(brand, channel = 1, volume = 50)
+    {
+        this.brand=brand;
+        this.channel=channel;
+        this.volume=volume;
+    }
+
+    getBrand()
+    {
+        return this.brand;
+    }
+
+    getChannel()
+    {
+        return this.channel;
+    }
+
+    getVolume()
+    {
+        return this.volume;
+    }
+
+    setVolume(input)
+    {   if(input > 100)
+        {
+          return this.volume = 100;
+        }
+        if(input < 0)
+        {
+            return this.volume = 0;
+        }
+        
+            return this.volume = input; 
+        
+        
+    }
+    setChannel(min,max)
+    {   
+        
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            this.channel= Math.floor(Math.random() * (max - min)) + min;
+          
+          
+    }
+
+    reset()
+    {
+        this.volume=50;
+        this.channel=1;
+        
+    }
+
+    status()
+    {
+        return `${this.brand} at channel ${this.channel}, volume ${this.volume}`
+    }
+}
+
+const myTv= new Tv('Panasonic', 8, 75);
+
+console.log('First output: ',myTv.status());
+
+myTv.setVolume(-25);
+console.log('After setVolume: ',myTv.status());
+
+myTv.setChannel(1,50);
+console.log('After randomly channel: ',myTv.status());
+
+myTv.reset();
+console.log('After reset: ',myTv.status());
